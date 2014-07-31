30431cfc8762e34958e195fe68e656c8fa9ebb55
Kookiat Suetrong (Beer) <kookiatsuetrong@gmail.com>
2014-08-01 05:03:41 +0700

    Basic word cloud

--- public/js/absolute.js
+++ public/js/absolute.js
@@ -89,6 +89,41 @@ function createCrossFilter(dataPath) {
 					.gap(1)
 					;
 			}
+			else
+			if (charts[i].type === 'wordcloud') {
+				d3.layout.cloud().size([width, height])
+					.words([
+						"Hello", "world", "normally", "you", "want", "more", "words",
+						"than", "this"].map(function(d) {
+						return {text: d, size: 10 + Math.random() * 90};
+					}))
+					.padding(5)
+					// .rotate(function() { return ~~(Math.random() * 2) * 90; })
+					.font("Impact")
+					.fontSize(function(d) { return d.size; })
+					.on("end", function (words) {
+						d3.select('#chart' + i).append("svg")
+							.attr("width", width)
+							.attr("height", height)
+							.append("g")
+							.attr("transform", "translate(" + width / 2 + "," +
+								height / 2 + ")")
+							.selectAll("text")
+							.data(words)
+							.enter().append("text")
+							.style("font-size", function(d) { return d.size +
+								"px"; })
+							.style("font-family", "Impact")
+							.style("fill", function(d, i) { return color(i); })
+							.attr("text-anchor", "middle")
+							.attr("transform", function(d) {
+								return "translate(" + [d.x, d.y] + ")rotate(" +
+									d.rotate + ")";
+							})
+							.text(function(d) { return d.text; });
+						})
+					.start();
+			}
 
 			if (charts[i].chart != null) {
 				charts[i].chart
@@ -127,7 +162,6 @@ function createCrossFilter(dataPath) {
 			.attr("transform", "translate(-10,0)rotate(315)");
 		*/
 	});
-
 }
 
 // TODO: Add right and bottom padding automatically
