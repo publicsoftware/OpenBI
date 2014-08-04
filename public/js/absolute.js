7617601ab3662ea7685d778b800cb04f832b0440
Kookiat Suetrong (Beer) <kookiatsuetrong@gmail.com>
2014-08-04 13:13:45 +0700

    Improve ruler

--- public/js/absolute.js
+++ public/js/absolute.js
@@ -238,35 +238,37 @@ function snap(x) {
 
 (function($) {
 	$.fn.draggable = function(opt) {
-
-		// opt = $.extend({handle:"",cursor:"move"}, opt);
-
 		if(opt.handle === "") {
-			var $el = this;
+			var el = this;
 		} else {
-			var $el = this.find(opt.handle);
+			var el = this.find(opt.handle);
 		}
 
-		return $el.css('cursor', opt.cursor).on("mousedown", function(e) {
+		return el.css('cursor', opt.cursor)
+		.on("mousedown", function(e) {
 			$(this).css('cursor', 'move');
-			$('.ruler').fadeIn();
 			if (opt.handle === "") {
-				var $drag = $(this).addClass('draggable');
+				var drag = $(this).addClass('draggable');
 			} else {
-				var $drag = $(this).addClass('active-handle').parent().
+				var drag = $(this).addClass('active-handle').parent().
 									addClass('draggable');
 			}
-			var z_idx = $drag.css('z-index'),
-				drg_h = $drag.outerHeight(),
-				drg_w = $drag.outerWidth(),
-				pos_y = $drag.offset().top + drg_h - e.pageY,
-				pos_x = $drag.offset().left + drg_w - e.pageX;
-			$drag.css('z-index', 900).parents().on("mousemove", function(e) {
+			var z = drag.css('z-index');
+			var drg_h = drag.outerHeight();
+			var drg_w = drag.outerWidth();
+			var pos_y = drag.offset().top + drg_h - e.pageY;
+			var pos_x = drag.offset().left + drg_w - e.pageX;
+
+			drag.css('z-index', 900).parents().on("mousemove", function(e) {
+				if ($('.draggable').length > 0) {
+					$('.ruler').fadeIn();
+				}
+
 				$('.draggable').offset({
 					top: snap(e.pageY + pos_y - drg_h),
 					left:snap(e.pageX + pos_x - drg_w)
 				}).on("mouseup", function() {
-					$(this).removeClass('draggable').css('z-index', z_idx);
+					$(this).removeClass('draggable').css('z-index', z);
 				});
 			});
 			e.preventDefault();
